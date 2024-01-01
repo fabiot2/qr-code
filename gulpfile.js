b71f321b04abeefcbd42ab9e46ce2dd85b06da15
Fabiot2 <fabiotriana90@gmail.com>
2023-12-31 22:22:43 -0500

    finishing qr code

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+// Define el origen y setea el destino del archivo 
+function buildStyles() {
+    return src('./sass/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('./css'))
+}
+
+// Cuando se guarda o aplican cambios a nuestro archivo, ejecuta la funcion para crear buildStyles
+function watchTask () {
+    watch(['./sass/**/*.scss'], buildStyles)
+}
+
+exports.default = series (buildStyles, watchTask)
